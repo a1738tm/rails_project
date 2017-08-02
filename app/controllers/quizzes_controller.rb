7b8747ae29c2ecebd13eff1fc26cdee92f26e2bf
murakami <a1738tm@aiit.ac.jp>
2017-08-02 19:40:39 +0900

    Added transition

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -6,6 +6,9 @@ class QuizzesController < ApplicationController
 
   # POST /quizzes/create
   def create
+    # render :action => "show"
+    @username = session[:username]
+    redirect_to :controller => "results", :action => "show"
   end
 
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,6 +2,7 @@ class ResultsController < ApplicationController
 
   # GET /result/show
   def show
+    @username = session[:username]
   end
 
 end
