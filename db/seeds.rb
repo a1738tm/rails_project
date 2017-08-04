d8f19ebc633f50f00735e049a92edcb3ea951e26
murakami <a1738tm@aiit.ac.jp>
2017-08-04 19:07:33 +0900

    Add initial user data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Result.delete_all
+Result.create(username: '回答者A', score: 0)
+Result.create(username: '回答者B', score: 10)
+Result.create(username: '回答者C', score: 20)
+Result.create(username: '回答者D', score: 30)
+Result.create(username: '回答者E', score: 40)
+Result.create(username: '回答者F', score: 50)
+Result.create(username: '回答者G', score: 60)
+Result.create(username: '回答者H', score: 70)
+Result.create(username: '回答者I', score: 80)
+Result.create(username: '回答者J', score: 90)
