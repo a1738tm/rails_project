37b73b9a1d38f15d1342356efc16c6f440922c1a
ttakaba <takabatoshiki@gmail.com>
2017-08-07 17:45:56 +0000

    get_rank

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,2 +1,15 @@
 class Result < ActiveRecord::Base
+
+def get_rank(score)
+   s = Result.pluck(:score)
+   if s.size != 0 then
+      s.push(score)
+      rank = s.map { |v| s.count { |a| a > v } + 1 } 
+      return rank[-1]
+   else
+      return 1
+   end
+end
+
+
 end
