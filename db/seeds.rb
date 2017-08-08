56d53cfc779ede3a42b893acd9e6813108a3a4cc
murakami <a1738tm@aiit.ac.jp>
2017-08-08 23:24:24 +0900

    Add some quizzes

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,23 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Result.delete_all
-Result.create(username: '回答者A', score: 0)
-Result.create(username: '回答者B', score: 10)
-Result.create(username: '回答者C', score: 20)
-Result.create(username: '回答者D', score: 30)
-Result.create(username: '回答者E', score: 40)
-Result.create(username: '回答者F', score: 50)
-Result.create(username: '回答者G', score: 60)
-Result.create(username: '回答者H', score: 70)
-Result.create(username: '回答者I', score: 80)
-Result.create(username: '回答者J', score: 90)
 
 Quiz.delete_all
-Quiz.create(question: '問題1', option1: '選択肢1-1', option2: '選択肢1-2', option3: '選択肢1-3', option4: '選択肢1-4', answer: 1)
-Quiz.create(question: '問題2', option1: '選択肢2-1', option2: '選択肢2-2', option3: '選択肢2-3', option4: '選択肢2-4', answer: 2)
-Quiz.create(question: '問題3', option1: '選択肢3-1', option2: '選択肢3-2', option3: '選択肢3-3', option4: '選択肢3-4', answer: 3)
-Quiz.create(question: '問題4', option1: '選択肢4-1', option2: '選択肢4-2', option3: '選択肢4-3', option4: '選択肢4-4', answer: 4)
-Quiz.create(question: '問題5', option1: '選択肢5-1', option2: '選択肢5-2', option3: '選択肢5-3', option4: '選択肢5-4', answer: 1)
+Quiz.create(question: '中華料理のバンバンジー。漢字で書くと？', option1: '棒棒鶏', option2: '万飯爺', option3: '万々慈', option4: '飯々餌', answer: 1)
+Quiz.create(question: '中華料理のザーサイ。感じで書くと？', option1: '在菜', option2: '搾菜', option3: '再菜', option4: '差菜', answer: 2)
+Quiz.create(question: '中華料理の搾菜。何と読む？', option1: 'シボナ', option2: 'シボサイ', option3: 'ザーサイ', option4: 'シーサイ', answer: 3)
+Quiz.create(question: '四字熟語「◯人◯色」、◯の数を足すといくつ？', option1: '5', option2: '10', option3: '15', option4: '20', answer: 4)
+Quiz.create(question: '最も緯度が低い都市はどこ？', option1: '香港', option2: '那覇', option3: '上海', option4: '台北', answer: 1)
 Quiz.create(question: '問題6', option1: '選択肢6-1', option2: '選択肢6-2', option3: '選択肢6-3', option4: '選択肢6-4', answer: 2)
 Quiz.create(question: '問題7', option1: '選択肢7-1', option2: '選択肢7-2', option3: '選択肢7-3', option4: '選択肢7-4', answer: 3)
 Quiz.create(question: '問題8', option1: '選択肢8-1', option2: '選択肢8-2', option3: '選択肢8-3', option4: '選択肢8-4', answer: 4)
